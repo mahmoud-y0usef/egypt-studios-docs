375dbe52c258cae9f531eeade5ef38718fa741f2
Mahmoud Yousef <68308514+mahmoud-y0usef@users.noreply.github.com>
2022-12-21 02:09:57 +0200

    ypdate

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -84,7 +84,7 @@ const config = {
             items: [
               {
                 label: 'about us',
-                to: '/docs/intro',
+                to: '/pages/rules',
               },
             ],
           },
