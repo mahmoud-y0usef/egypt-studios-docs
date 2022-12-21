16331d948ddc9d6015dd253c166d1c7610bdd0bc
Mahmoud Yousef <68308514+mahmoud-y0usef@users.noreply.github.com>
2022-12-21 06:26:36 +0200

    ttt

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -65,7 +65,7 @@ const config = {
         items: [
           {
             type: 'doc',
-            docId: 'intro',
+            // docId: 'intro',
             position: 'left',
             label: 'esx scripts',
           },
