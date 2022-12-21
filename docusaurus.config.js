b525217cb6765df43e18db2bcc78e6702820c47c
Mahmoud Yousef <68308514+mahmoud-y0usef@users.noreply.github.com>
2022-12-21 03:06:24 +0200

    update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -84,7 +84,7 @@ const config = {
             items: [
               {
                 label: 'about us',
-                to: '/pages/rules',
+                to: '/docs/rules.md',
               },
             ],
           },
