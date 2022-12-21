aaab3617289330d7e4b06c73df0bf447e3f314f3
Mahmoud Yousef <68308514+mahmoud-y0usef@users.noreply.github.com>
2022-12-21 04:37:35 +0200

    updates

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -5,7 +5,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'home',
+  title: '|egypt-studios',
   tagline: `Welcome to Egypt Studio 
     The goal : programming new things that affect the community of the fivem or more, and other things`,
   url: 'https://egypt-studios-docs.vercel.app/',
