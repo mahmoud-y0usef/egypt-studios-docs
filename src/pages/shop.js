9e064aca44afc82dfb81eabfe484c8174828bd49
Mahmoud Yousef <68308514+mahmoud-y0usef@users.noreply.github.com>
2022-12-22 00:30:10 +0200

    5555

--- src/pages/shop.js
+++
@@ -1,7 +0,0 @@
-// function prepareFrame() {
-//     var ifrm = document.createElement("iframe");
-//     ifrm.setAttribute("src", "https://egyptstudio.tebex.io/category/2031240");
-//     ifrm.style.width = "100%";
-//     ifrm.style.height = "100%";
-//     document.body.appendChild(ifrm);
-// }
