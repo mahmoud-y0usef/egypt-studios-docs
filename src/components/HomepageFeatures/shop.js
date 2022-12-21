53755de940f4e20f50fb9ad9b195400f9cfdd84d
Mahmoud Yousef <68308514+mahmoud-y0usef@users.noreply.github.com>
2022-12-22 00:37:37 +0200

    go now

---
+++ src/components/HomepageFeatures/shop.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import clsx from 'clsx';
+import styles from './styles.module.css';
+import Iframe from 'react-iframe'
+<Iframe url="https://egyptstudio.tebex.io/category/2031240"
+        width="100%"
+        height="100%"
+        className="shop-now"
+        display="block"
+        position="relative"/>
